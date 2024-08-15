d82855acdeaa94b7bbe2ff1d8bc6b57607331a1d
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-08-15 16:42:39 -0300

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://images.pexels.com/photos/1472999/pexels-photo-1472999.jpeg?auto=compress&cs=tinysrgb&w=600"
+        alt="Imagem Tela Inicial"
+        className="img"
+      />
+    </>
+  );
+}
+
+export default Home;
