b68e282394622e5eb299f8eca2575be1870cc2dc
Sara Oliveira <sara.monteiro1234@gmail.com>
2024-08-16 11:17:48 -0300

    inclusão de rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
- 
-  
-
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+      <div className="w-full bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
+          <div className="text-2xl font-bold uppercase">Blog Pessoal</div>
 
-            <div className='flex gap-4'>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
-            </div>
+          <div className="flex gap-4">
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+            <Link to="/home" className="hover:underline">
+              Home
+            </Link>
+            <div className="hover:underline">Postagens</div>
+            <div className="hover:underline">Temas</div>
+            <div className="hover:underline">Cadastrar tema</div>
+            <div className="hover:underline">Perfil</div>
+            <div className="hover:underline">Sair</div>
           </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import homeLogo from '../../assets/home.png'
-import './Home.css';
-
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <>
-        <div className="bg-indigo-900 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
-              <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
-            </div>
-  
-            <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
-      
+  return (
+    <>
+      <div className="bg-gradient-to-r from-pink-500 to-purple-500">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">
+              Oi! Que bom ter você está aqui!
+            </h2>
+            <p className="text-xl">
+              Participe das nossas discussões sobre músicas, filmes e séries!
+            </p>
+
+            <div className="flex justify-around gap-4">
+              <button className="rounded bg-white text-blue-800 py-2 px-4">
+                Ver postagens
+              </button>
             </div>
           </div>
+
+          <div className="flex justify-center ">
+            <img
+              src="https://img.pikbest.com/png-images/qiantu/original-music-listening-girl-illustration_2737011.png!sw800"
+              alt=""
+              className="w-2/3"
+            />
+          </div>
         </div>
-      
-      </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Home;
